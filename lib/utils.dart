c360c3e8f33b1524e97ea98f3a07b91405dd0abd
Gogetter <euedofia@gmail.com>
2019-06-01 17:16:53 +0100

    updated

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,8 +1,11 @@
+import 'model.dart';
 
+String extractThumbnail(String url, MessageType type) {
+  if (type != MessageType.Video) {
+    return url;
+  } else {
+    String base_url = "https://img.youtube.com/vi/%/0.jpg";
+    return base_url.replaceAll("%", url.split("?v=")[1]);
+  }
 
-String extractThumbnail(String url) {
-  String base_url = "https://img.youtube.com/vi/%/0.jpg";
-  return base_url.replaceAll("%", url.split("?v=")[1]);
 }
-
-
