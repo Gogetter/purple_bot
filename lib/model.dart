9423992a68c7075d74d89286805a26f1b6e2c389
Gogetter <euedofia@gmail.com>
2019-06-01 16:41:26 +0100

    add Message type to model

--- lib/model.dart
+++ lib/model.dart
@@ -4,23 +4,32 @@ enum ChatMessageFrom {
   AutoReply,
 }
 
+enum MessageType {
+  Text,
+  Video,
+  Image
+}
+
 class ChatMessage {
   final ChatMessageFrom from;
   final String text;
   final String link;
+  final MessageType messageType;
   final List<String> replies;
 
   const ChatMessage.forAutoReply(this.replies)
       : this.from = ChatMessageFrom.AutoReply,
         this.link = null,
+        this.messageType = MessageType.Text,
         this.text = null;
 
-  const ChatMessage.fromServer(this.text,[this.link])
+  const ChatMessage.fromServer(this.text,[this.link, this.messageType])
       : this.from = ChatMessageFrom.Server,
         this.replies = null;
 
   const ChatMessage.fromMyself(this.text)
       : this.from = ChatMessageFrom.Myself,
         this.link = null,
+        this.messageType = MessageType.Text,
         this.replies = null;
 }
