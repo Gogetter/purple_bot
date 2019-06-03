a9d14243d251a1a120ad189197fdd5dc4cf57642
Gogetter <euedofia@gmail.com>
2019-06-03 20:06:25 +0100

    removed dead code

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,6 +1,3 @@
-import 'package:flutter/material.dart';
-import 'package:rich_link_preview/rich_link_preview.dart';
-
 import 'model.dart';
 
 String extractThumbnail(String url, MessageType type) {
@@ -10,26 +7,4 @@ String extractThumbnail(String url, MessageType type) {
     String base_url = "https://img.youtube.com/vi/%/0.jpg";
     return base_url.replaceAll("%", url.split("?v=")[1]);
   }
-
-  Widget messageTypeWidget(String link, MessageType messageType) {
-    switch (messageType) {
-      case MessageType.Image: {
-        return Image.network(extractThumbnail(link, messageType));
-      }
-
-      break;
-      case MessageType.Video: {
-        return RichLinkPreview(
-          link: link,
-          appendToLink: true,
-        );
-      }
-      break;
-
-      case MessageType.Text: {
-
-      }
-    }
-  }
-
 }
